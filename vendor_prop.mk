a9870d2fcaabcf9cd27cf3c8b61c407b633ba130
tangrobin <tangrobin@google.com>
2020-06-08 23:11:12 +0800

    lavender: use phase offsets as duration
    In Android R, the SurfaceFlinger calculate phase offsets
    based on duration. Adopt the new way of specifying
    phase offsets to SurfaceFlinger.
    
    Bug: 158464992
    Test: scrolling, app launching/closing, playing video
    Change-Id: Ia717f5c0075b6adaedaf20dd6be689737edc1415

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -277,3 +277,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #disable dual isp sync by default
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.vendor.camera.dual.isp.sync=0
+
+# Phase offsets
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.use_phase_offsets_as_durations=1 \
+    debug.sf.late.sf.duration=10500000 \
+    debug.sf.late.app.duration=20500000 \
+    debug.sf.early.sf.duration=21000000 \
+    debug.sf.early.app.duration=16500000 \
+    debug.sf.earlyGl.sf.duration=13500000 \
+    debug.sf.earlyGl.app.duration=21000000
