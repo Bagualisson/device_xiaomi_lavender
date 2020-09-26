5d3b4d41a9a5709d585a5ed65034b56b7955b83f
AlissonGrizotti <alisson1397@hotmail.com>
2020-09-26 21:11:17 +0000

    Lavender: Drop ANT Radio library

--- device.mk
+++ device.mk
@@ -129,15 +129,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
-
 # Binder
 PRODUCT_PACKAGES += \
     libhwbinder \
